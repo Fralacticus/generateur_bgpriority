3fbe64566595c8adc9c6713cbfd14690ae6ca041
Fralacticus <delcourt.r@gmail.com>
2022-02-06 10:41:02 +0100

    2eme version

--- bin/generateur_bgpriority.dart
+++ bin/generateur_bgpriority.dart
@@ -3,29 +3,39 @@ import "dart:io";
 import 'dart:convert';
 
 int main(List<String> arguments) {
-
+  print("Début du programme generateur_bgpriority\n");
   int couleurPrio = 4281204937; //#C9002E
 
   String chemin_assets = arguments[0];
   String nomDossier_bgpriority = r"bgpriority";
 
   // Vérification si le dossier existe
+  stdout.write("1) Vérification existence dossier bgpriority ... ");
   Directory dossier_bgpriority = Directory(chemin_assets + r"\" + nomDossier_bgpriority);
   if(dossier_bgpriority.existsSync() == false){
     throw "Le dossier n'existe pas : ${dossier_bgpriority.path}";
   }
+  stdout.writeln("OK");
 
   // Vidage du dossier des fichiers json
+  stdout.writeln("2) Suppression des fichiers .json :");
   List<FileSystemEntity> listeFichiers_bgpriority  = dossier_bgpriority.listSync().where((element) => FileSystemEntity.isFileSync(element.path) && element.path.split('.').last == "json").toList();
   listeFichiers_bgpriority.forEach((fichiers) {
+    stdout.write("Supression de ${fichiers.path} ... ");
     fichiers.deleteSync(recursive: true);
+    stdout.writeln("Supprimé");
   });
 
+
   // Lecture fichiers png
+  stdout.write("3) Inventaire des fichiers .png du dossier bgpriority ... ");
   List<FileSystemEntity>  listeFichiers_png  = dossier_bgpriority.listSync().where((element) => FileSystemEntity.isFileSync(element.path) && element.path.split('.').last == "png").toList();
+  stdout.writeln("Fait");
 
   // Traitement de chaque fichier png
+  stdout.writeln("4) Traitements et Génération des fichiers de priorité :");
   listeFichiers_png.forEach((fichier) {
+    stdout.write("${fichier.path} ... ");
     Image pngOrigi = decodePng( File(fichier.path).readAsBytesSync())!;
     if(!estConforme(pngOrigi)){
       throw "L'image nest pas conforme : ${fichier.path}";
@@ -33,9 +43,12 @@ int main(List<String> arguments) {
     List<List<int>> imgDecoupe = decouperImage(pngOrigi);
     List<bool>listePriorite =  genererPriorite(imgDecoupe, couleurPrio);
     String jsonPriorite = genererJson(listePriorite);
-    File(dossier_bgpriority.path + r"\" + fichier.path.split(r'\').last.split(r'.').first + ".json").writeAsStringSync(jsonPriorite);
+    String cheminSortie = dossier_bgpriority.path + r"\" + fichier.path.split(r'\').last.split(r'.').first + ".json";
+    File(cheminSortie).writeAsStringSync(jsonPriorite);
+    stdout.writeln("Traité");
   });
 
+  print("\nProgramme terminé !");
   return 0;
 }
 
